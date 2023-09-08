a9d08624f2a0c6d48b438c1f61cfd698c81f99a5
Chris Stewart <stewart.chris1234@gmail.com>
2023-09-08 15:51:01 -0500

    Implement OP_ADD64, OP_SUB64, OP_MUL64, OP_DIV64, OP_LESSTHAN64, OP_LESSTHANOREQUAL64, OP_GREATERTHAN64, OP_GREATERTHANOREQUAL64, OP_SCRIPTNUMTOLE64, OP_LE64TOSCRIPTNUM, OP_LE32TOLE64
    Remove liquid args
    
    WIP
    
    Get simple OP_1 functional test case working
    
    Get tests for arithmetic and comparison opcodes working
    
    Get all functional tests passing
    
    Rename test case to Arithmetic64bitTest
    
    Rename file to feature_64bit_arithmetic_opcodes.py, add it to test_runner.py
    
    Get tests passing in feature_taproot.py
    
    Remove unused push_le4
    
    Revert test fixture setup
    
    Cleanup
    
    Fix linting
    
    test: Add leaf_version parameter to taproot_tree_helper()
    
    Fix bug
    
    Fix bugs
    
    Fix compile
    
    Fix missing sigversion checks
    
    Fix htole64 -> htole64_internal due to #29263
    
    Add negative test case for OP_LE64TOSCRIPTNUM
    
    Use OP_EQUAL rather than OP_EQUALVERIFY
    
    Fix rebase

--- test/functional/feature_64bit_arithmetic_opcodes.py
+++ test/functional/feature_64bit_arithmetic_opcodes.py
@@ -9,7 +9,11 @@ from test_framework.util import assert_raises_rpc_error
 from test_framework.key import compute_xonly_pubkey, generate_privkey
 from test_framework.messages import COutPoint, CTransaction, CTxIn, CTxInWitness, CTxOut, ser_uint256, sha256, tx_from_hex
 from test_framework.test_framework import BitcoinTestFramework
+<<<<<<< HEAD
 from test_framework.script import CScript, CScriptNum, CScriptOp, OP_1, OP_ADD64, OP_SUB64, OP_MUL64, OP_DIV64, OP_LESSTHAN64, OP_LESSTHANOREQUAL64, OP_GREATERTHAN64, OP_GREATERTHANOREQUAL64, OP_NEG64, OP_SCRIPTNUMTOLE64, OP_LE64TOSCRIPTNUM, OP_LE32TOLE64, OP_DUP, OP_EQUAL, OP_EQUALVERIFY, OP_VERIFY, taproot_construct, SIGHASH_DEFAULT, SIGHASH_ALL, SIGHASH_NONE, SIGHASH_SINGLE, SIGHASH_ANYONECANPAY, LEAF_VERSION_TAPSCRIPT_64BIT
+=======
+from test_framework.script import OP_8, OP_DROP, OP_SIZE, CScript, CScriptNum, CScriptOp, OP_1, OP_ADD64, OP_SUB64, OP_MUL64, OP_DIV64, OP_LESSTHAN64, OP_LESSTHANOREQUAL64, OP_GREATERTHAN64, OP_GREATERTHANOREQUAL64, OP_NEG64, OP_SCRIPTNUMTOLE64, OP_LE64TOSCRIPTNUM, OP_LE32TOLE64, OP_DUP, OP_EQUAL, OP_EQUALVERIFY, OP_VERIFY, taproot_construct, SIGHASH_DEFAULT, SIGHASH_ALL, SIGHASH_NONE, SIGHASH_SINGLE, SIGHASH_ANYONECANPAY, LEAF_VERSION_TAPSCRIPT_64BIT
+>>>>>>> 2c40ae01e5 (Implement OP_ADD64, OP_SUB64, OP_MUL64, OP_DIV64, OP_LESSTHAN64, OP_LESSTHANOREQUAL64, OP_GREATERTHAN64, OP_GREATERTHANOREQUAL64, OP_SCRIPTNUMTOLE64, OP_LE64TOSCRIPTNUM, OP_LE32TOLE64)
 from test_framework.address import output_key_to_p2tr
 
 VALID_SIGHASHES_ECDSA = [
@@ -96,7 +100,11 @@ class Arithmetic64bitTest(BitcoinTestFramework):
 
         tx = CTransaction()
 
+<<<<<<< HEAD
         tx.version = ver
+=======
+        tx.nVersion = ver
+>>>>>>> 2c40ae01e5 (Implement OP_ADD64, OP_SUB64, OP_MUL64, OP_DIV64, OP_LESSTHAN64, OP_LESSTHANOREQUAL64, OP_GREATERTHAN64, OP_GREATERTHANOREQUAL64, OP_SCRIPTNUMTOLE64, OP_LE64TOSCRIPTNUM, OP_LE32TOLE64)
         tx.nLockTime = locktime
         # Spend the pegin and taproot tx together
         in_total = prev_tx.vout[prev_vout].nValue #.getAmount()
