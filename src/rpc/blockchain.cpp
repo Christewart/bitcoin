d93af454c221bec84703a43ccb387d1f3fe39da0
James O'Beirne <james.obeirne@pm.me>
2025-03-19 16:16:58 -0400

    consensus: add a CSFS deployment for regtest only

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1425,7 +1425,7 @@ UniValue DeploymentInfo(const CBlockIndex* blockindex, const ChainstateManager& 
     SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_TAPROOT);
 
     if (chainman.GetParams().GetChainType() == ChainType::REGTEST) {
-        SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_CTV);
+        SoftForkDescPushBack(blockindex, softforks, chainman, Consensus::DEPLOYMENT_CTVCSFS);
     }
     return softforks;
 }
